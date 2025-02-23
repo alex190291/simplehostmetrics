26233455b052d8c42fc7889222898eacf96e46e0
alex190291 <alex190291@hotmail.com>
2025-02-23 04:15:09 +0100

    +

--- static/theme.js
+++ static/theme.js
@@ -1,12 +1,19 @@
 // static/theme.js
 
-const modeToggle = document.getElementById("modeToggle");
-modeToggle.addEventListener("click", () => {
-  document.body.classList.toggle("light-mode");
+document.addEventListener("DOMContentLoaded", () => {
   const mapElement = document.getElementById("map");
-  if (document.body.classList.contains("light-mode")) {
-    mapElement.classList.remove("dark-invert");
-  } else {
+  // Standardmäßig Dark Mode aktivieren, wenn nicht im Light Mode
+  if (!document.body.classList.contains("light-mode")) {
     mapElement.classList.add("dark-invert");
   }
+
+  const modeToggle = document.getElementById("modeToggle");
+  modeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+    if (document.body.classList.contains("light-mode")) {
+      mapElement.classList.remove("dark-invert");
+    } else {
+      mapElement.classList.add("dark-invert");
+    }
+  });
 });
