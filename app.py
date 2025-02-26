64f34cbf445f92846f4a4ad9ec27e43c84371b4b
alex190291 <alex190291@hotmail.com>
2025-02-26 06:05:57 +0100

    +

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
 from flask_assets import Environment, Bundle
 from flask_scss import Scss
+from webassets.script import CommandLineEnvironment
 import threading
 import logging
 import time
@@ -8,6 +9,7 @@ import yaml
 import requests
 import docker
 from urllib.parse import urljoin
+
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_security.utils import hash_password
@@ -352,15 +354,18 @@ Scss(app, static_dir='static', asset_dir='scss')
 threading.Thread(target=start_rtad_log_parser, daemon=True).start()
 
 with app.app_context():
-    assets.build()
+    # Build assets: use app.static_folder as the output directory for compiled assets.
+    cl_env = CommandLineEnvironment(assets, app.static_folder)
+    cl_env.build()
     threading.Thread(target=stats.update_stats_cache, daemon=True).start()
     threading.Thread(target=docker_manager.docker_info_updater, daemon=True).start()
     threading.Thread(target=docker_manager.check_image_updates, daemon=True).start()
     threading.Thread(target=rtad_manager.update_country_info_job, daemon=True).start()
 
 if __name__ == '__main__':
-    # Manually trigger the build of assets
-    assets.build()
+    # Build assets: use app.static_folder as the output directory for compiled assets.
+    cl_env = CommandLineEnvironment(assets, app.static_folder)
+    cl_env.build()
 
     # Start background threads
     threading.Thread(target=stats.update_stats_cache, daemon=True).start()
